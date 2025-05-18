38c52a6e147c5b0fe1a9d5ec2062dad2d5bd4660
João Pedro <jpedrocruz78@gmail.com>
2025-05-18 11:10:38 -0400

    add book constructor

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-const title = document.querySelector('h1');
-title.style.color = "blue";
+//initialize book array
+const myLibrary = [];
 
 // constructor book function
+function Book (title, year,genre, author, id) {
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+    this.author = author;
+    this.id = id;
+};
+
+// function to add books to the aarray
+function addBookToLibrary(title, year, genre, author) {
+    const id = crypto.randomUUID();
+    const book = new book(title,year,genre,author,id);
+    myLibrary.push(book);
+}
 
 
